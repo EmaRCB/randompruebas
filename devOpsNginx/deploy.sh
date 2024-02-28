eb9154ed2bea166355424cbfd0b5111db61dc806
Emanuel Rafael Chávez Bagundo <A17003636@alumnos.uady.mx>
2024-02-28 17:00:55 -0600

    Update deploy.sh

--- devOpsNginx/deploy.sh
+++ devOpsNginx/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Detener NGINX
-sudo systemctl stop nginx
+sudo systemctl stop systemd-escape nginx
 # Detener NGROK
 pkill ngrok
 # Clonar repositorio
 cd ..
 git pull
 # Encender NGINX y NGROK
-sudo systemctl start nginx
+sudo systemctl start systemd-escape nginx
 # Extrae el url de NGROK y mostrarlo
 ngrok_url=$(echo "ngrok http 8080" | awk '/^Forwarding/ {print $2}')
 echo "NGROK URL: $ngrok_url"
